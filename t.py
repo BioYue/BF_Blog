d833a07212532b6d4c96687f62eafbc8e9f73b22
Yue <pyueren@gmail.com>
2023-12-08 14:51:47 +0800

    归档页面及逻辑完成

--- t.py
+++ t.py
@@ -1,36 +1,5 @@
-a = [{'id': 17, 'content': '222', 'visitor_name': '222', 'add_time': datetime.datetime(2023, 12, 7, 22, 15, 3),
-      'replies': [
-          {'id': 18, 'content': '333', 'visitor_name': '333', 'add_time': datetime.datetime(2023, 12, 7, 22, 15, 3),
-           'replies': [
-               {'id': 19, 'content': '444', 'visitor_name': '444',
-                'add_time': datetime.datetime(2023, 12, 7, 22, 15, 3),
-                'replies': []}]}]},
-     {'id': 18, 'content': '333', 'visitor_name': '333', 'add_time': datetime.datetime(2023, 12, 7, 22, 15, 3),
-      'replies': [
-          {'id': 19, 'content': '444', 'visitor_name': '444', 'add_time': datetime.datetime(2023, 12, 7, 22, 15, 3),
-           'replies': []}]},
-     {'id': 19, 'content': '444', 'visitor_name': '444', 'add_time': datetime.datetime(2023, 12, 7, 22, 15, 3),
-      'replies': []},
-     {'id': 16, 'content': '111', 'visitor_name': '111', 'add_time': datetime.datetime(2023, 12, 7, 22, 13, 18),
-      'replies': [
-          {'id': 17, 'content': '222', 'visitor_name': '222', 'add_time': datetime.datetime(2023, 12, 7, 22, 15, 3),
-           'replies': [{'id': 18, 'content': '333', 'visitor_name': '333',
-                        'add_time': datetime.datetime(2023, 12, 7, 22, 15, 3), 'replies': [
-                   {'id': 19, 'content': '444', 'visitor_name': '444',
-                    'add_time': datetime.datetime(2023, 12, 7, 22, 15, 3), 'replies': []}]}]}]},
-     {'id': 12, 'content': '父级评论1', 'visitor_name': '22', 'add_time': datetime.datetime(2023, 12, 7, 19, 58, 31),
-      'replies': [
-          {'id': 14, 'content': '子评论1', 'visitor_name': '22', 'add_time': datetime.datetime(2023, 12, 7, 19, 58, 31),
-           'replies': []},
-          {'id': 16, 'content': '111', 'visitor_name': '111', 'add_time': datetime.datetime(2023, 12, 7, 22, 13, 18),
-           'replies': [
-               {'id': 17, 'content': '222', 'visitor_name': '222',
-                'add_time': datetime.datetime(2023, 12, 7, 22, 15, 3),
-                'replies': [{'id': 18, 'content': '333', 'visitor_name': '333',
-                             'add_time': datetime.datetime(2023, 12, 7, 22, 15, 3), 'replies': [
-                        {'id': 19, 'content': '444', 'visitor_name': '444',
-                         'add_time': datetime.datetime(2023, 12, 7, 22, 15, 3), 'replies': []}]}]}]}]},
-     {'id': 13, 'content': '父级评论2', 'visitor_name': '33', 'add_time': datetime.datetime(2023, 12, 7, 19, 58, 31),
-      'replies': []},
-     {'id': 14, 'content': '子评论1', 'visitor_name': '22', 'add_time': datetime.datetime(2023, 12, 7, 19, 58, 31),
-      'replies': []}]
+
+a = {}
+a['base'].append('2')
+a.setdefault('base',[]).append('2')
+print(a)
